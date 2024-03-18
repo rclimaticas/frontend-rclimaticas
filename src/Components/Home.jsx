2061c43ada142b3162b12b85934d20bad27c2c59
jvittor <162454027+rclimaticas@users.noreply.github.com>
2024-03-18 14:54:25 -0300

    :recycle: add: grid organizations

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,10 +1,34 @@
 import React from "react";
-import { Center } from "@chakra-ui/react";
+import { Box, Image, Grid, GridItem } from "@chakra-ui/react";
+import Main from "./main/main";
+
 const Home = () => {
   return (
-    <div>
-      <Center h="100vh">hello world!</Center>
-    </div>
+    <>
+      <Grid
+    templateAreas={`"header header"
+                    "nav main"
+                    "nav footer"`}
+    gridTemplateRows={'50px 100fr 70px'}
+    gridTemplateColumns={'0 1fr 0px'}
+    h='500px'
+    gap='0'
+    color='black'
+    fontWeight='normal'
+  >
+    <GridItem pl='2' bg='white' area={'header'}>
+      Header
+    </GridItem>
+    <GridItem pl='2' bg='#219394' height={'1980px'} area={'main'}>
+      <Main />
+    </GridItem>
+    <GridItem pl='2' bg='blue.300' area={'footer'}>
+      Footer
+    </GridItem>
+  </Grid>
+      
+      
+    </>
   );
 };
 
