262e7012b87a27947165158daeb44ff28a1562e8
jvittor <162454027+rclimaticas@users.noreply.github.com>
2024-03-22 18:56:02 -0300

    :recycle: add: navbar and change color in the main

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import { Box, Image, Grid, GridItem } from "@chakra-ui/react";
 import Main from "./main/main";
+import Navbar from "./header/navbar";
 
 const Home = () => {
   return (
     <>
       <Grid
     templateAreas={`"header header"
-                    "nav main"
-                    "nav footer"`}
-    gridTemplateRows={'50px 100fr 70px'}
-    gridTemplateColumns={'0 1fr 0px'}
-    h='500px'
-    gap='0'
+                    "nav main"`}
+    gridTemplateRows="50px 1fr 0px"
+    gridTemplateColumns={'150px 1fr'}
+    h={{lg: '8900px', xl: '8400px', '2xl': '8200px'}}
+    gap={10}
     color='black'
+    bg={'#219394'}
     fontWeight='normal'
   >
-    <GridItem pl='2' bg='white' area={'header'}>
-      Header
+    <GridItem pl='2' bg='#219394' area={'header'}>
+      <Navbar />
     </GridItem>
-    <GridItem pl='2' bg='#219394' height={'1980px'} area={'main'}>
+    <GridItem pl='2' bg='#219394' area={'main'}>
       <Main />
-    </GridItem>
-    <GridItem pl='2' bg='blue.300' area={'footer'}>
-      Footer
     </GridItem>
   </Grid>
       
