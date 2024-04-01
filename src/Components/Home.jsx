535483a9a5d5435aa68375093e1dfae6594443dd
jvittor <162454027+rclimaticas@users.noreply.github.com>
2024-04-01 14:07:11 -0300

    :recycle: refactor: changing home page design

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -3,29 +3,28 @@ import { Box, Image, Grid, GridItem } from "@chakra-ui/react";
 import Main from "./main/main";
 import Navbar from "./header/navbar";
 
+
 const Home = () => {
   return (
     <>
       <Grid
-    templateAreas={`"header header"
-                    "nav main"`}
-    gridTemplateRows="50px 1fr 0px"
-    gridTemplateColumns={'150px 1fr'}
-    h={{lg: '8900px', xl: '8400px', '2xl': '8200px'}}
-    gap={10}
-    color='black'
-    bg={'#219394'}
-    fontWeight='normal'
-  >
-    <GridItem pl='2' bg='#219394' area={'header'}>
-      <Navbar />
-    </GridItem>
-    <GridItem pl='2' bg='#219394' area={'main'}>
-      <Main />
-    </GridItem>
-  </Grid>
-      
-      
+        templateAreas={`"header header"
+                        "nav main"
+                      `}
+        gridTemplateRows="50px 1fr 0px"
+        gridTemplateColumns={'150px 1fr'}
+        gap={10}
+        height="900px"
+       // Definindo a altura da Grid para ocupar toda a altura da tela
+        fontWeight='normal'
+      >
+        <GridItem pl='2' area={'header'}>
+          <Navbar />
+        </GridItem>
+        <GridItem area={'main'}>
+          <Main />
+        </GridItem>
+      </Grid>
     </>
   );
 };
