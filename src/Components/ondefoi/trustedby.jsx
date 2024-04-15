e010575374d7103641fc3b63eab8e946445934b2
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-15 13:18:48 -0300

    :sparkles: feat: trustedby on ondefoi's page

---
+++ src/Components/ondefoi/trustedby.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Box, Center } from '@chakra-ui/react';
+
+export default function About() {
+    return (
+        <>
+        <Center>
+            <Box
+            mt="8rem"
+            bg="orange"
+            w="70rem"
+            h="200px"
+            ></Box>
+        </Center>
+
+        </>
+    );
+}
