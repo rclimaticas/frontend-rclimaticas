13f87863e2a1d3d1f22ea1dfb923a1eae2006220
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-15 13:17:43 -0300

    :recycle: add: adding ondefoi, data rc, login and register page routes on app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,10 @@ import React from "react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
-
+import OndeFoi from "./Components/ondefoi/ondefoi";
+import DataRC from "./Components/datarc/datarc";
+import Login from "./Components/Form/login";
+import Register from "./Components/Form/register";
 
 const App = () => {
   return (
@@ -11,6 +14,11 @@ const App = () => {
         <Routes>
           {/* Alterar caminho da página de login */}
           <Route path="/" element={<Home />} />
+          <Route path="/ondefoi" element={<OndeFoi />} />
+          <Route path="/datarc" element={<DataRC />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+
         </Routes>
       </BrowserRouter>
     </ChakraProvider>
