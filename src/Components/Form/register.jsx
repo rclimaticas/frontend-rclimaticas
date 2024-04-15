9567fc7336817475aa7df8f0386873f7f79ec2ec
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-15 13:16:40 -0300

    :sparkles: feat: login and register page

---
+++ src/Components/Form/register.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {} from '@chakra-ui/react';
+
+export default function Register() {
+    return (
+        <div>
+            <h1>Register</h1>
+        </div>
+    );
+}
