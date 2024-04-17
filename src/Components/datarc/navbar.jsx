2691e98614ac12dd7bc0fac205823838fbb9e34d
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-17 20:34:25 -0300

    :recycle: refactor: add ref links

--- src/Components/datarc/navbar.jsx
+++ src/Components/datarc/navbar.jsx
@@ -18,9 +18,15 @@ export default function navbar() {
                 fontFamily={"Arial"}
                 fontSize={"20px"}
                 >
-                    <Text _hover={{ color: "#30806e" }} cursor="pointer" >Dados e Informações</Text>
-                    <Text _hover={{ color: "#30806e" }} cursor="pointer">Registros OndeFoi</Text>
-                    <Text _hover={{ color: "#30806e" }} cursor="pointer">Assesoria Sofia</Text>
+                    <a href="/datarc">
+                        <Text _hover={{ color: "#30806e" }} cursor="pointer" >Dados e Informações</Text>
+                    </a>
+                    <a href="/ondefoi">
+                        <Text _hover={{ color: "#30806e" }} cursor="pointer">Registros OndeFoi</Text>
+                    </a>
+                    <a href="https://www.espiralds.com/sofia">
+                        <Text _hover={{ color: "#30806e" }} cursor="pointer">Assesoria Sofia</Text>
+                    </a>
                     <Text _hover={{ color: "#30806e" }} cursor="pointer">Liga Colaborativa</Text>
                 </HStack>
             </Box>
