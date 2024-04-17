3629d8eecbabddad80806b0fc8bce1f96bc0a55b
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-17 20:29:26 -0300

    :sparkles: feat: DataRC, OndeFoi, Login, Register  Routes/GoogleAuth Provider

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,30 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import OndeFoi from "./Components/ondefoi/ondefoi";
 import DataRC from "./Components/datarc/datarc";
-import Login from "./Components/Form/login";
-import Register from "./Components/Form/register";
-
+import Login from "./Components/form/login";
+import Register from "./Components/form/register";
+import { AuthProvider } from './Components/hooks/authcontext'; // Importe o AuthProvider aqui
+import { UserProvider } from './Components/hooks/userContext';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 const App = () => {
   return (
     <ChakraProvider>
-      <BrowserRouter>
-        <Routes>
-          {/* Alterar caminho da página de login */}
-          <Route path="/" element={<Home />} />
-          <Route path="/ondefoi" element={<OndeFoi />} />
-          <Route path="/datarc" element={<DataRC />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-
-        </Routes>
-      </BrowserRouter>
+      <UserProvider>
+        <AuthProvider>
+          <GoogleOAuthProvider clientId="121933231345-24jcpdkc5ck4og584uou6fd9mcnbtgpe.apps.googleusercontent.com">
+            <BrowserRouter>
+              <Routes>
+                {/* Alterar caminho da página de login */}
+                <Route path="/" element={<Home />} />
+                <Route path="/ondefoi" element={<OndeFoi />} />
+                <Route path="/datarc" element={<DataRC />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+              </Routes>
+            </BrowserRouter>
+          </GoogleOAuthProvider>
+        </AuthProvider>
+      </UserProvider>
     </ChakraProvider>
   );
 };
