b235480f758274b42626e9310d3d5b41324f67fa
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-17 20:32:02 -0300

    :recycle: refactor: add ref links maps

--- src/Components/datarc/data.maps.jsx
+++ src/Components/datarc/data.maps.jsx
@@ -28,63 +28,71 @@ export default function DataMaps() {
                     >
                         <Heading mt={"2rem"} pos={"relative"} right={"17rem"}>Plataformas de Dados e Mapas</Heading>
                             <HStack spacing={"5rem"} mt={"5rem"}>
-                                <Box
-                                    w={"500px"}
-                                    h="300px"
-                                    onMouseEnter={() => handleMouseEnter('data1')}
-                                    onMouseLeave={handleMouseLeave}
-                                    style={{ opacity: hoveredBox === 'data1' || hoveredBox === null ? 1 : 0.7}}
-                                    transition="transform 0.3s"
-                                    _hover={{ transform: "scale(1.05)" }}
-                                    as="button"
-                                >
-                                    <Image src={Data1} w="100%" h="100%"/>
-                                    <Heading mt={2} fontSize={"25px"} textAlign={"center"}>Dados e Mapas sobre Energia</Heading>
-                                </Box>
+                                <a href='https://gisepeprd2.epe.gov.br/WebMapEPE/'>
+                                    <Box
+                                        w={"500px"}
+                                        h="300px"
+                                        onMouseEnter={() => handleMouseEnter('data1')}
+                                        onMouseLeave={handleMouseLeave}
+                                        style={{ opacity: hoveredBox === 'data1' || hoveredBox === null ? 1 : 0.7}}
+                                        transition="transform 0.3s"
+                                        _hover={{ transform: "scale(1.05)" }}
+                                        as="button"
+                                    >
+                                        <Image src={Data1} w="100%" h="100%"/>
+                                        <Heading mt={2} fontSize={"25px"} textAlign={"center"}>Dados e Mapas sobre Energia</Heading>
+                                    </Box>
+                                </a>
                                 
-                                <Box
-                                    w={"500px"}
-                                    h="300px"
-                                    onMouseEnter={() => handleMouseEnter('data2')}
-                                    onMouseLeave={handleMouseLeave}
-                                    style={{ opacity: hoveredBox === 'data2' || hoveredBox === null ? 1 : 0.7}}
-                                    transition="transform 0.3s"
-                                    _hover={{ transform: "scale(1.05)" }}
-                                    as="button"
-                                >
-                                    <Image src={Data2} w="100%" h="100%"/>
-                                    <Heading mt={2} fontSize={"25px"} textAlign={"center"}>Dados e Mapas Mapbiomas</Heading>
-                                </Box>
+                                <a href="https://brasil.mapbiomas.org/">
+                                    <Box
+                                        w={"500px"}
+                                        h="300px"
+                                        onMouseEnter={() => handleMouseEnter('data2')}
+                                        onMouseLeave={handleMouseLeave}
+                                        style={{ opacity: hoveredBox === 'data2' || hoveredBox === null ? 1 : 0.7}}
+                                        transition="transform 0.3s"
+                                        _hover={{ transform: "scale(1.05)" }}
+                                        as="button"
+                                    >
+                                        <Image src={Data2} w="100%" h="100%"/>
+                                        <Heading mt={2} fontSize={"25px"} textAlign={"center"}>Dados e Mapas Mapbiomas</Heading>
+                                    </Box>
+                                </a>
                                 
                             </HStack>
                             <HStack spacing={"5rem"} mt={"5rem"}>
-                                <Box
-                                    w={"500px"}
-                                    h="300px"
-                                    onMouseEnter={() => handleMouseEnter('data3')}
-                                    onMouseLeave={handleMouseLeave}
-                                    style={{ opacity: hoveredBox === 'data3' || hoveredBox === null ? 1 : 0.7}}
-                                    transition="transform 0.3s"
-                                    _hover={{ transform: "scale(1.05)" }}
-                                    as="button"
-                                >
-                                    <Image src={Data3} w="100%" h="100%"/>
-                                    <Heading mt={2} fontSize={"25px"} textAlign={"center"}>Dados e Mapas sobre Mineração</Heading>
-                                </Box>
-                                <Box
-                                    bg="orange"
-                                    w={"500px"}
-                                    h="300px"
-                                    onMouseEnter={() => handleMouseEnter('data4')}
-                                    onMouseLeave={handleMouseLeave}
-                                    style={{ opacity: hoveredBox === 'data4' || hoveredBox === null ? 1 : 0.7}}
-                                    transition="transform 0.3s"
-                                    _hover={{ transform: "scale(1.05)" }}
-                                    as="button"
-                                >
-                                    <Image src={Data4} w={"100%"} h="100%" />
-                                    <Heading mt={2} fontSize={"25px"} textAlign={"center"}>Dados e Mapas do IBGE</Heading>
-                                </Box>
+                                <a href="https://www.sgb.gov.br/p3m/">
+                                    <Box
+                                        w={"500px"}
+                                        h="300px"
+                                        onMouseEnter={() => handleMouseEnter('data3')}
+                                        onMouseLeave={handleMouseLeave}
+                                        style={{ opacity: hoveredBox === 'data3' || hoveredBox === null ? 1 : 0.7}}
+                                        transition="transform 0.3s"
+                                        _hover={{ transform: "scale(1.05)" }}
+                                        as="button"
+                                    >
+                                        <Image src={Data3} w="100%" h="100%"/>
+                                        <Heading mt={2} fontSize={"25px"} textAlign={"center"}>Dados e Mapas sobre Mineração</Heading>
+                                    </Box>
+                                </a>
+                                <a href='https://portaldemapas.ibge.gov.br/portal.php#homepage'>
+                                    <Box
+                                        bg="orange"
+                                        w={"500px"}
+                                        h="300px"
+                                        onMouseEnter={() => handleMouseEnter('data4')}
+                                        onMouseLeave={handleMouseLeave}
+                                        style={{ opacity: hoveredBox === 'data4' || hoveredBox === null ? 1 : 0.7}}
+                                        transition="transform 0.3s"
+                                        _hover={{ transform: "scale(1.05)" }}
+                                        as="button"
+                                    >
+                                        <Image src={Data4} w={"100%"} h="100%" />
+                                        <Heading mt={2} fontSize={"25px"} textAlign={"center"}>Dados e Mapas do IBGE</Heading>
+                                    </Box>
+                                </a>
                             </HStack>
                     </VStack>
             </Box>
