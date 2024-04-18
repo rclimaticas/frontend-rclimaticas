fbaea09fbdf17b5a5fc3587f3a8633364d80d5da
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-17 21:19:34 -0300

    :recycle: add: Trusted by / Collaborate Photo

--- src/Components/ondefoi/trustedby.jsx
+++ src/Components/ondefoi/trustedby.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Box, Center } from '@chakra-ui/react';
-
+import { Box, Center, Heading, HStack, Image } from '@chakra-ui/react';
+import Trusted from '../../assets/trusted.png'
+import Financiadores from '../../assets/financiadores.png'
 export default function About() {
     return (
         <>
+       
         <Center>
-            <Box
-            mt="8rem"
-            bg="orange"
-            w="70rem"
-            h="200px"
-            ></Box>
+            <HStack spacing={"5rem"} mt="8rem">
+                
+                <Box
+                w="30rem"
+                h="320px"
+                
+                >
+                    <Heading color="white" fontFamily={"Chiller"} fontSize={"50px"} textAlign={"left"} mb={5}>TrustedBy</Heading>
+                    <Image borderRadius={"2rem"} boxShadow={"-10px 10px 0px #40EC51"} src={Trusted} />
+                </Box>
+                <Box
+                mt="4rem"
+                w="40rem"
+                h="200px"
+                >
+                    <Heading color="white" fontFamily={"Alata"} textAlign={"center"}>Colabore</Heading>
+                    <Box boxShadow={"-10px 10px 0px #40EC51"} borderRadius={"20rem"} bg="white">
+                        <Image mt={6} src={Financiadores} p={5}/>
+                    </Box>
+                </Box>
+            </HStack>
         </Center>
 
         </>
