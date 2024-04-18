afd462c28d410f0a4638b9089e6b83781ea9c46a
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-18 14:29:38 -0300

    :recycle: change: rename Login and Register paste/directory

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,30 +1,49 @@
-import React from "react";
-import { Box, Image, Grid, GridItem } from "@chakra-ui/react";
+import React, { useState, useEffect } from "react";
+import { Box, Image, Grid, GridItem, Center} from "@chakra-ui/react";
 import Main from "./main/main";
 import Navbar from "./header/navbar";
-
+import './styles/loading.css'
 
 const Home = () => {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    // limpando o timer
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <Grid
-        templateAreas={`"header header"
+      {loading ? (
+        <Center h="100vh">
+          <Box className="loader"></Box>
+        </Center>
+      ) : (
+        <>
+          <Grid
+            templateAreas={`"header header"
                         "nav main"
                       `}
-        gridTemplateRows="50px 1fr 0px"
-        gridTemplateColumns={'150px 1fr'}
-        gap={10}
-        height="900px"
-       // Definindo a altura da Grid para ocupar toda a altura da tela
-        fontWeight='normal'
-      >
-        <GridItem pl='2' area={'header'}>
-          <Navbar />
-        </GridItem>
-        <GridItem area={'main'}>
-          <Main />
-        </GridItem>
-      </Grid>
+            gridTemplateRows="50px 1fr 0px"
+            gridTemplateColumns={'150px 1fr'}
+            gap={10}
+            height="900px"
+            // Definindo a altura da Grid para ocupar toda a altura da tela
+            fontWeight='normal'
+          >
+            <GridItem pl='2' area={'header'}>
+              <Navbar />
+            </GridItem>
+            <GridItem area={'main'}>
+              <Main />
+            </GridItem>
+          </Grid>
+        </>
+      )}
     </>
   );
 };
