f5fcd4c94d8ccecce7a34b5a4be13ccc1630fbc6
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-18 14:36:01 -0300

    :sparkles: add: contribution (DataRC Page)

---
+++ src/Components/datarc/contribution.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import { Text, Container, Box, Wrap, HStack, Heading, Image, Stack, InputGroup, InputLeftAddon, Input, Button } from '@chakra-ui/react';
+import Trusted from '../../assets/trusted.png'
+import Financiadores from '../../assets/financiadores.png'
+
+export default function Contribution() {
+    return (
+        <>
+            <HStack spacing={20} p={"10rem"}>
+                <Box w="500px" h="400px">
+                    <Stack
+                        spacing={4}>
+                        <Text
+                            fontSize={"18px"}
+                            fontFamily={"Arial"}
+                        >Receba Atualizações</Text>
+                        <InputGroup>
+                            <InputLeftAddon>
+                                🫵
+                            </InputLeftAddon>
+                            <Input
+                                border="1px solid"
+                                bg={"#eced95"}
+                                type='tel' placeholder='Digite seu nome' />
+                        </InputGroup>
+                        <InputGroup>
+                            <InputLeftAddon>
+                                📫
+                            </InputLeftAddon>
+                            <Input
+                                border="1px solid"
+                                bg={"#eced95"}
+                                type='tel' placeholder='Email' />
+                        </InputGroup>
+                    </Stack>
+                    <Button
+                        mt={5}
+                        w={"30%"}
+                        _hover={{ bg: "white" }}
+                        bg={'white'}
+                        boxShadow={"-4px 4px 4px rgba(0, 0, 0, 0.4)"}
+                    >
+                        <Text
+                            fontSize={"18px"}
+                        >
+                            Enviar
+                        </Text>
+                    </Button>
+                    <Stack p={10}>
+                        <Text p={10} fontSize={"18px"} fontFamily={"Arial"} textAlign={"center"}>
+                                <u>
+                                    Contribua com o projeto financiado
+                                </u>
+                        </Text>
+                        <Box
+                        w={"100%"}
+                        h="100px"
+                        >
+                            <Image src={Financiadores}/>
+
+                        </Box>
+                    </Stack>
+                </Box>
+                <Box
+                    w="500px"
+                    h="400px"
+                >
+                    <Heading color="black" fontFamily={"Chiller"} fontSize={"50px"} textAlign={"right"} mb={5}>TrustedBy</Heading>
+                    <Image borderRadius={"2rem"} src={Trusted} />
+                </Box>
+            </HStack>
+        </>
+    )
+}
