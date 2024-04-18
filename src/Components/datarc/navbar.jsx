1144d678ca3a708f59f60b7e3f8511a2784fe741
jvittor <102699222+jvittor@users.noreply.github.com>
2024-04-18 14:41:04 -0300

    :sparkles: feat: integration back-end on navbar/token access

--- src/Components/datarc/navbar.jsx
+++ src/Components/datarc/navbar.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-import { Box, HStack, Text } from "@chakra-ui/react";
+import { Box, HStack, Text, Menu, MenuButton, MenuList, MenuItem, Button } from "@chakra-ui/react";
+import { useAuth } from '../hooks/authcontext'; 
 
 export default function navbar() {
+
+    const { token, logout } = useAuth(); // acessa o token do contexto de autenticação
+    const handleLogout = () => {
+        // chama a função de logout do contexto de autenticação, se estiver disponível
+        if (logout) {
+            logout(); // chama a função de logout
+        }
+        // limpa o token armazenado localmente
+        localStorage.removeItem('token');
+        // redireciona o usuário para a página inicial
+        window.location.href = '/';
+    };
 
     return (
         <>
@@ -28,6 +41,31 @@ export default function navbar() {
                         <Text _hover={{ color: "#30806e" }} cursor="pointer">Assesoria Sofia</Text>
                     </a>
                     <Text _hover={{ color: "#30806e" }} cursor="pointer">Liga Colaborativa</Text>
+                    {token ? ( // Verifica se há um token (usuário logado)
+                    <Menu>
+                        <MenuButton as={Button} bg="transparent" _hover={{ color: "#30806e" }}>
+                            <Text fontFamily={"Alata"}></Text>
+                            Minha Conta
+                        </MenuButton>
+                        <MenuList>
+                            <MenuItem>Perfil</MenuItem>
+                            <MenuItem>Configurações</MenuItem>
+                            <MenuItem onClick={handleLogout}>Sair</MenuItem>
+                        </MenuList>
+                    </Menu>
+                ) : (
+                    <HStack pos="relative" left={"8rem"}>
+                        <a href={'/login'}>
+                            <Button bg={"#CFD242"}
+                            _hover={{bg: "#bdbf47"}}
+                            border={"2px"}
+                            >Login</Button>
+                        </a>
+                        <a href='/register'>
+                            <Button bg="white" _hover={{color: "#30806e"}}>Sign up</Button>
+                        </a>
+                    </HStack>
+                )}
                 </HStack>
             </Box>
         </>
