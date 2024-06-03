07a305e0855b096ed9a5ea7eb37eb11da97cb2ac
jvittor <102699222+jvittor@users.noreply.github.com>
2024-06-03 18:36:58 -0300

    :bug: refactor: name import's

--- src/Components/ondefoi/index.jsx
+++ src/Components/ondefoi/index.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import Hero from './hero.jsx'
+import Hero from './Hero.jsx'
 import About from './about.jsx'
 import Header from '../../common/header.jsx'
-import Strategies from './strategies.jsx'
-import Dashboard from './dashboard.jsx'
-import TrustedBy from './trustedBy.jsx'
+import Strategies from './Strategies.jsx'
+import Dashboard from './Dashboard.jsx'
+import TrustedBy from './TrustedBy.jsx'
 
 export default function Index() {
     const [imageLoad, setImageLoad] = useState(false);
