89ac8482251c97d7c967eb770d42bc68466aacc3
jvittor <102699222+jvittor@users.noreply.github.com>
2024-06-03 18:34:17 -0300

    :recycle: refactor: rename ondefoi pages

--- src/Components/ondefoi/index.jsx
+++ src/Components/ondefoi/index.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import Hero from './Hero.jsx'
-import About from './About.jsx'
+import Hero from './hero.jsx'
+import About from './about.jsx'
 import Header from '../../common/header.jsx'
-import Strategies from './Strategies.jsx'
-import Dashboard from './Dashboard.jsx'
-import TrustedBy from './TrustedBy.jsx'
+import Strategies from './strategies.jsx'
+import Dashboard from './dashboard.jsx'
+import TrustedBy from './trustedBy.jsx'
 
 export default function Index() {
     const [imageLoad, setImageLoad] = useState(false);
