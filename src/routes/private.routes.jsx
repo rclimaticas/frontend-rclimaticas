70de87b786bc905d196fea9c7da5a806a8c7ff42
jvittor <102699222+jvittor@users.noreply.github.com>
2024-06-03 18:30:00 -0300

    :recycle: refactor: adding routes and providers to public and private routes

--- src/routes/private.routes.jsx
+++ src/routes/private.routes.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import UserProfile from '../Components/user/userProfile.jsx';
-import OndeFoi from "../Components/ondefoi/ondefoi.jsx";
 import DataRC from "../Components/datarc/datarc.jsx";
 import Login from "../Components/form/Login.jsx";
 import Register from "../Components/form/Register.jsx";
@@ -10,23 +9,27 @@ import Home from "../Components/home/home.jsx";
 import LigaColaborativa from "../Components/ligacolaborativa/lc.jsx";
 import theme from "../Components/styles/theme.ts";
 import { GoogleOAuthProvider } from '@react-oauth/google';
+import { AccountSettingsProvider } from '../Components/context/AccountSettingsContext.jsx';
+import OndeFoi from '../Components/ondefoi/index.jsx';
 
 const PrivateRoutes = () => {
     return (
         <ChakraProvider theme={theme}>
-            <GoogleOAuthProvider clientId="121933231345-24jcpdkc5ck4og584uou6fd9mcnbtgpe.apps.googleusercontent.com">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Home />} />
-                        <Route path="/ondefoi" element={<OndeFoi />} />
-                        <Route path="/datarc" element={<DataRC />} />
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/register" element={<Register />} />
-                        <Route path="/ligacolaborativa" element={<LigaColaborativa />} />
-                        <Route path="/user" element={<UserProfile />} />
-                    </Routes>
-                </BrowserRouter>
-            </GoogleOAuthProvider>
+            <AccountSettingsProvider>
+                <GoogleOAuthProvider clientId="121933231345-24jcpdkc5ck4og584uou6fd9mcnbtgpe.apps.googleusercontent.com">
+                    <BrowserRouter>
+                        <Routes>
+                            <Route path="/" element={<Home />} />
+                            <Route path="/datarc" element={<DataRC />} />
+                            <Route path="/login" element={<Login />} />
+                            <Route path="/register" element={<Register />} />
+                            <Route path="/ligacolaborativa" element={<LigaColaborativa />} />
+                            <Route path="/user" element={<UserProfile />} />
+                            <Route path="/ondefoi" element={<OndeFoi />} />
+                        </Routes>
+                    </BrowserRouter>
+                </GoogleOAuthProvider>
+            </AccountSettingsProvider>
         </ChakraProvider>
     );
 };
--- src/routes/public.routes.jsx
+++ src/routes/public.routes.jsx
@@ -3,7 +3,6 @@ import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import SimpleBar from 'simplebar-react';
 import 'simplebar-react/dist/simplebar.min.css';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import OndeFoi from "../Components/ondefoi/ondefoi.jsx";
 import DataRC from "../Components/datarc/datarc.jsx";
 import Login from "../Components/form/Login.jsx";
 import Register from "../Components/form/Register.jsx";
@@ -11,24 +10,29 @@ import Home from "../Components/home/home.jsx";
 import LigaColaborativa from "../Components/ligacolaborativa/lc.jsx";
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import theme from "../Components/styles/theme.ts";
+import { AccountSettingsProvider } from '../Components/context/AccountSettingsContext.jsx';
+import OndeFoi from '../Components/ondefoi/index.jsx';
+
 const PublicRoutes = () => {
 
   return (
-      <ChakraProvider theme={theme}>
+    <ChakraProvider theme={theme}>
+      <AccountSettingsProvider>
         <GoogleOAuthProvider clientId="121933231345-24jcpdkc5ck4og584uou6fd9mcnbtgpe.apps.googleusercontent.com">
           <BrowserRouter>
             <Routes>
               {/* Alterar caminho da página de login */}
               <Route path="/" element={<Home />} />
-              <Route path="/ondefoi" element={<OndeFoi />} />
               <Route path="/datarc" element={<DataRC />} />
               <Route path="/login" element={<Login />} />
               <Route path="/register" element={<Register />} />
               <Route path="/ligacolaborativa" element={<LigaColaborativa />} />
+              <Route path="/ondefoi" element={<OndeFoi />} />
             </Routes>
           </BrowserRouter>
         </GoogleOAuthProvider>
-      </ChakraProvider>
+      </AccountSettingsProvider>
+    </ChakraProvider>
   );
 };
 export default PublicRoutes;
